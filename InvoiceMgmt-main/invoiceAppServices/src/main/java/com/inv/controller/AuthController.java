b67af2dc2b58656c64db5396a83cdad70e38047f
Gokulmty-Work <gklmoorthy.work@gmail.com>
2024-06-06 01:29:29 +0530

    Update crossorigin

--- InvoiceMgmt-main/invoiceAppServices/src/main/java/com/inv/controller/AuthController.java
+++ InvoiceMgmt-main/invoiceAppServices/src/main/java/com/inv/controller/AuthController.java
@@ -35,7 +35,7 @@ package com.inv.controller;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.http.HttpStatus;
 	import org.springframework.http.ResponseEntity;
-//	import org.springframework.web.bind.annotation.CrossOrigin;
+	import org.springframework.web.bind.annotation.CrossOrigin;
 	import org.springframework.web.bind.annotation.DeleteMapping;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.PathVariable;
@@ -49,7 +49,7 @@ package com.inv.controller;
 
 
 
-//	@CrossOrigin(origins = "*")
+	@CrossOrigin(origins = {"http://localhost:4200", "https://invoicemgmt.onrender.com"})
 	@RestController
 	@RequestMapping("/auth")   
 
